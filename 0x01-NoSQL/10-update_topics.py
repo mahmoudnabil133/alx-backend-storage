6274db859930748f25f725832edd2aea896010f9
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-17 10:11:28 -0700

    update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -10,4 +10,4 @@ def update_topics(mongo_collection, name, topics):
     in mongo documents
     return None
     """
-    mongo_collection.update_one({name: name}, {$set: {"topics": topics}})
+    mongo_collection.update_many({name: name}, {$set: {"topics": topics}})
