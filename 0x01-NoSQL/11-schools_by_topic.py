8a1e8f2bd40616f4ef271d42622eab8059417d44
mahmoudnabil133 <hodanabil144@gmail.com>
2024-06-17 11:14:32 -0700

    find_topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -3,9 +3,5 @@
 return scholl have specific topics
 """
 def schools_by_topic(mongo_collection, topic):
-    "find topic"
-    ret = []
-    for doc in mongo_collection.find():
-        if topic in doc.get("topics"):
-            ret.append(doc)
-    return doc
+    "find toopics"
+    return mongo_collection.find({topic: topic})
